81c6a1473f7b42400ba3354a1719b20120c46535
Tomar <ankitomar@deloitte.com>
2025-01-27 21:15:03 +0530

    Initial commit

---
+++ Day 1/task/BandNameGeneratorProject.py
@@ -0,0 +1,4 @@
+print("Namaste!")
+city = input("Please enter your city - \n")
+petName = input("Enter your pet name - \n")
+print("Your Band Name is - " + city + " " + petName)
---
+++ Day 1/task/VariableSwap.py
@@ -0,0 +1,13 @@
+glass1 = "milk"
+glass2 = "juice"
+print("Before Swap")
+print("Glass 1 is " + glass1)
+print("Glass 2 is " + glass2)
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+print("After Swap")
+print("Glass 1 is " + glass1)
+print("Glass 2 is " + glass2)
